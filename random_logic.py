aa67a51b13b886689b54a840df96dd46bc1d2c76
DmitryResh <dmitriireshetnev@gmail.com>
2018-09-30 01:51:49 +0500

    Added conditional

--- random_logic.py
+++ random_logic.py
@@ -2,9 +2,12 @@ import random
 
 print("Welcome To The My Randomiser \nSet The Range Of Random Number")
 
-enter_max = int(input("Enter maximum edge "))
+enter_max = int(input("Enter maximum "))
 
-enter_min = int(input("Enter minimum edge "))
+enter_min = int(input("Enter minimum "))
+
+if enter_max <= enter_min:
+	raise SystemExit("Maximum value must be more than Minimum")
 
 number = int(random.uniform(enter_min, enter_max))
 print(number)
